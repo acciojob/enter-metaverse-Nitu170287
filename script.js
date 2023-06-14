f4740f248902fb4f9538e3ca33ad5ea45d545d98
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-14 14:49:20 -0400

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 let newString = "Entered Metaverse"
 function changeString(){
 	let para = document.getElementById("status")
+	let head = document.createElement("h1")
 	para.innerText = `${newString}`
-	para.style.fontWeight = "bold"
-	para.style.fontSize = "28px"
+	head.textContent = para.textContent
+	para.parentNode.replaceChild(head, para);
+	// para.innerText = `${newString}`
+	// para.style.fontWeight = "bold"
+	// para.style.fontSize = "28px"
 	
 }
